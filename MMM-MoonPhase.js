c23e144a30c0cd10a3bc30ecb33b10e49aec29d5
Nolan Kingdon <nolan.kingdon@gmail.com>
2019-06-21 18:29:50 -0400

    Added in trigonometry to create realistic curve

--- MMM-MoonPhase.js
+++ MMM-MoonPhase.js
@@ -14,8 +14,8 @@ Module.register("MMM-MoonPhase", {
 	//	resolution: "detailed",
 		title: true, //Whether or not the Moon Phase Title is displayed
 		phase: true, //Label for what moon phase it is
-		x: "200",
-		y: "200"
+		x: 200,
+		y: 200
 	},
 
 	requiresVersion: "2.1.0", // Required version of MagicMirror
@@ -38,95 +38,170 @@ Module.register("MMM-MoonPhase", {
 	getDom: function() {
 		var self = this;
 
-		// create element wrapper for show into the module
+		// create element wrapper
 		let wrapper = document.createElement("div");
 		wrapper.id = "moonphase-wrapper";
 		let title   = document.createElement("p");
 		title.id    = "moonphase-title";
-		//title.style.borderBottom = "1px solid white";
-		if(this.config.title) {title.innerHTML = this.translate("TITLE");}else{title.style.display="none";}
+		// If title is turned on - Create Title
+		if(this.config.title) {
+			title.innerHTML = this.translate("TITLE");
+		} else {
+			title.style.display="none";
+		}
+		// Create Canvas
 		let moonCanvas = document.createElement("canvas");
 		moonCanvas.id  = "moonphase-canvas";
 		moonCanvas.height = this.config.y;
 		moonCanvas.width = this.config.x;
+		//Adding in the background img
 		moonCanvas.style.background = "url('https://github.com/NolanKingdon/MMM-MoonPhase/blob/master/images/Phases/full.png?raw=true')";
 		moonCanvas.style.backgroundSize = "cover";
+		//Adding in our moon phase for below the moon
 		let phase   = document.createElement("p");
 		phase.id    = "moonphase-phase";
-		if(!this.config.phase){phase.style.display = "none";}
-		this.drawCanvas(moonCanvas, phase);
-		//Appending our empty elements to the DOM object
+	
+		if(!this.config.phase){ // Hiding the title if turned off in config
+			phase.style.display = "none";
+		}
+		//Drawing on the existing canvas
+		this.drawCanvas(phase, moonCanvas);
+		//Appending our elements to the DOM object
 		wrapper.appendChild(title);
 		wrapper.appendChild(moonCanvas);
 		wrapper.appendChild(phase);
 		return wrapper;
 	},
 
-	drawCanvas: function(canvas, phase){
-		let ctx = canvas.getContext("2d");
-		const X = parseInt(this.config.x);
-        	const Y = parseInt(this.config.y);
-		const BEZ_HEIGHT = Y/10;
+	drawPoint: function(coor, ctx){
+		// If the points z coordinate is less than
+		// zero, it is out of view thus, grey.
+		if (coor[2] >= 0) color = "rgb(0,0,0)";
+		else color = "transparent";
+		ctx.fillStyle = color;
+		ctx.lineTo(this.config.x/2 + coor[0], this.config.y/2 - coor[1]);
+		ctx.stroke();
+	},
+	
+    // Degree to radians
+	radians: function(angle) {
+		return angle * (Math.PI / 180);
+	},
+	
+	// Convert spherical coordinate to x y z coordinate
+	sphericalToPoint: function(ascension, declination) {
+		ascension = this.radians(ascension);
+		declination = this.radians(declination);
+		return [
+			Math.sin(ascension) * Math.sin(declination) * r,
+			Math.cos(declination) * r,
+			Math.cos(ascension) * Math.sin(declination) * r
+		];
+	},
+	
+	// Turn for x (0), y (1) or z (2) axis
+	rotateForAxis: function(axis, coor, angle) {
+		angle = this.radians(angle);
+		static = coor.splice(axis, 1)[0];
+		c1 = coor[0];
+		c2 = coor[1];
 
-		let day = this.calculatePhase();
-		let roundedDay = Math.floor(day);
+		coor = [
+			Math.cos(angle) * c1 - Math.sin(angle) * c2,
+			Math.sin(angle) * c1 + Math.cos(angle) * c2
+		];
+
+		coor.splice(axis, 0, static);
+
+		return coor;
+	 },
+	
+	// Turn for all axis rotations
+	rotate: function(coor) {
+		return this.rotateForAxis(
+			2, this.rotateForAxis(
+				1, this.rotateForAxis(
+					0, coor, rotate_a[0]), rotate_a[1]), rotate_a[2]);
+	},
+	
+	// Draw three axis circles
+	drawAxisCircles: function(jDate, ctx) {
+		// Clear canvas
+		ctx.clearRect(0, 0, this.config.x, this.config.y);
+		ctx.beginPath();
+		
+		//Applying blur to our stroke		
+		if(jDate[1] < 15 && jDate[1] > 0){
+			ctx.shadowOffsetX = 7;
+			ctx.shadowOffsetY = -1;
+			ctx.shadowColor = "black";
+			ctx.shadowBlur = 10;
+		}else if(jDate[1] >= 16 && jDate[1] < 29){
+			ctx.shadowOffsetX = -7;
+			ctx.shadowOffsetY = -1;
+			ctx.shadowColor = "black";
+			ctx.shadowBlur = 10;
+		}
+		
+		for (i = 0; i < 180; i += increase_by) {
+			this.drawPoint(this.rotate(this.sphericalToPoint(90, i)), ctx);
+		}
+		if(jDate[1] < 15){// We are in waxing phases
+			ctx.lineTo(0, this.config.y);
+			ctx.lineTo(0, this.config.y);
+			ctx.lineTo(0,0);
+			ctx.fill();
+		} else if(jDate[1] >= 15){
+			ctx.lineTo(this.config.x, this.config.y);
+			ctx.lineTo(this.config.x, 0);
+			ctx.lineTo(this.config.x/2, 0);
+			ctx.fill();
+		}
+	},
+	
+	drawCanvas: function(phase, canvas){
+		//let testDay = 2; //[testDay%15, testDay];
+   	    let jDate = this.getMoonPhase();
+
+	    var d_canvas = canvas;//document.getElementById('canvas');
+	    var ctx = d_canvas.getContext('2d');
+	    increase_by = 2;
+	    r = this.config.x/2; // radius
+	    rotate_a = [0, 360*(jDate[0]/29.5), 0]; // rotation angles
+		
+		//Starting the chain to draw the current curve of the moon
+		
+		/* NOTE - Math was heavily referenced from here:
+		 * 
+		 * https://medium.com/@refik/a-journey-and-a-method-for-drawing-spheres-5b24246ca479
+		 * https://github.com/refik/rotate-sphere
+		 * 
+		 * A great writeup + the code tool he mentions in the article. These have been a great help
+		 * to me.
+		 */
+		this.drawAxisCircles(jDate, ctx);
+		
 		//There's definitely a better way to do this.
-		if(roundedDay == 0 || roundedDay == 29){
+		if(jDate[1] == 0 || jDate[1] == 29){
 			phase.innerHTML = this.translate("NEW");
-		}else if(roundedDay > 0 && roundedDay < 7){
+		}else if(jDate[1] > 0 && jDate[1] < 7){
 			phase.innerHTML = this.translate("WAX_CRESC");
-		}else if(roundedDay == 7) {
+		}else if(jDate[1] == 7) {
 			phase.innerHTML = this.translate("FIRST");
-		}else if(roundedDay > 7 && roundedDay < 15){
+		}else if(jDate[1] > 7 && jDate[1] < 15){
 			phase.innerHTML = this.translate("WAX_GIB");
-		}else if(roundedDay == 15){
+		}else if(jDate[1] == 15){
 			phase.innerHTML = this.translate("FULL");
-		}else if(roundedDay > 15 && roundedDay < 22){
+		}else if(jDate[1] > 15 && jDate[1] < 22){
 			phase.innerHTML = this.translate("WAN_GIB");
-		}else if(roundedDay == 22) {
+		}else if(jDate[1] == 22) {
 			phase.innerHTML = this.translate("THIRD");
-		}else if(roundedDay > 22 && roundedDay < 29){
+		}else if(jDate[1] > 22 && jDate[1] < 29){
 			phase.innerHTML = this.translate("WAN_CRESC");
 		}
 
-		let dayMod;
-
-		if(day < 16){
-			dayMod = day;
-		} else if(day >= 16){
-			dayMod = day%15;
-		}
-		Log.log(dayMod);
-		Log.log(day);
-		let bezCover = X*1.15;
-		let bezDif = bezCover - X/2;
-		let step = 7;
-		let bezX = bezCover-( bezDif / step * dayMod);
-
-		if(day < 15 && day > 0){
-			ctx.shadowOffsetX = 7;
-			ctx.shadowColor = "black";
-			ctx.shadowBlur = 10;
-		}else if(day >= 16 && day < 29){
-			ctx.shadowOffsetX = -7;
-			ctx.shadowColor = "black";
-			ctx.shadowBlur = 10;
-		}
-
-		ctx.beginPath();
-		ctx.moveTo(X/2, -10);
-		ctx.bezierCurveTo(bezX, BEZ_HEIGHT, bezX, Y-BEZ_HEIGHT, X/2, Y+10);
-		if(day < 16){
-			ctx.lineTo(0,Y);
-			ctx.lineTo(0,0);
-			ctx.lineTo(X/2, 0);
-			ctx.fill();
-		} else {
-			ctx.lineTo(X, Y);
-			ctx.lineTo(X, 0);
-			ctx.lineTo(X/2, 0);
-			ctx.fill();
-		}
+	
+		// Transforming the moon image to align with the southern hemisphere
 		if(this.config.hemisphere.toUpperCase() === "S"){canvas.style.transform = "rotate(180deg)";}
 
 	},
@@ -146,9 +221,7 @@ Module.register("MMM-MoonPhase", {
 		};
 	},
 
-	calculatePhase: function(){
-		//Formula from here: https://www.subsystems.us/uploads/9/8/9/4/98948044/moonphase.pdf
-		//Getting Current Date
+	getMoonPhase: function(){ // Gets the current Julian date
 		const currDate = new Date();
 		let d = currDate.getDate();
 		let m = currDate.getMonth()+1;
@@ -172,8 +245,10 @@ Module.register("MMM-MoonPhase", {
 		let moonFraction  = "0." + newMoons.toString().split(".")[1];
 		//Our final Digit - 29.53 days a moon cycle. 15 is full moon. 0/29.5 is new
 		let dayOfCycle    = parseFloat(moonFraction * 29.53);
-		return dayOfCycle;
-	},
+		let dayOfCycleMod = dayOfCycle%15; // Allows us to utilize the 180deg math below --> Use the same curve twice = less calc
+		//Returning our modded day AND our actual day
+		return [dayOfCycleMod, dayOfCycle];
+  },
 	// socketNotificationReceived from helper
 	socketNotificationReceived: function (notification, payload) {
 		if(notification === "MMM-MoonPhase-PHASE-RECIEVED") {
